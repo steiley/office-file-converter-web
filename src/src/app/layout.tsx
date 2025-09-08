a7fb2e924be1ded549fa90ff480263fc0fef9ffb
steiley <steiley.4106@gmail.com>
2025-09-08 15:36:51 +0900

    Introduce i18n

--- src/src/app/layout.tsx
+++ src/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import {NextIntlClientProvider} from 'next-intl';
+ 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
     </html>
   );
